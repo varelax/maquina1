988d2b8a7161ac5ee4aa596bcb70d9db69b42f43
Jose Plaza <alumno@profesor-ord82>
2015-04-30 09:52:36 +0200

    prueba subida botesss

---
+++ src/maquinabebidas/DispensadorBotes.java
@@ -0,0 +1,89 @@
+
+
+import java.util.*;
+
+public class DispensadorBotes {
+
+    private int boteCola=15;
+    private int boteLimon=15;
+    private int boteNaranja=15;
+    private int boteTonica=15;
+    private int boteAgua=15;
+    
+    
+    
+    public void IniciarDispensadorDeBotes (int i){
+        
+        Scanner tec=new Scanner(System.in);
+        int opcion;
+        boolean monedaIn = true;
+        boolean stock= true;
+        do{
+            System.out.println("MENU");
+            System.out.println("---------------------");
+            System.out.println("1- Cola");
+            System.out.println("2- Limon");
+            System.out.println("3- Naranja");
+            System.out.println("4- Tonica");
+            System.out.println("5- Agua");
+            System.out.println("0- ");
+            opcion=tec.nextInt();
+            switch(opcion){
+                case 1:
+                    if(boteCola!=0){
+                        System.out.println(stock);
+                    }else{
+                        stock=false;
+                        System.out.println(stock);
+                    }
+                    
+                    break;
+                   
+                case 2:
+                     if(boteLimon!=0){
+                        System.out.println(stock);
+                    }else{
+                        stock=false;
+                        System.out.println(stock);
+                    }
+                    break;
+                case 3:
+                     if(boteNaranja!=0){
+                        System.out.println(stock);
+                    }else{
+                        stock=false;
+                        System.out.println(stock);
+                    }
+                    break;
+                case 4:
+                     if(boteTonica!=0){
+                        System.out.println(stock);
+                    }else{
+                        stock=false;
+                        System.out.println(stock);
+                    }
+                    break;
+                case 5:
+                     if(boteAgua!=0){
+                        System.out.println(stock);
+                    }else{
+                        stock=false;
+                        System.out.println(stock);
+                    }
+                    break;
+                case 0:
+                    
+                    monedaIn = false;
+                    break;
+            }
+                
+             
+        }while(monedaIn=true);
+        
+    }
+    public boolean PulsarBoton(){
+        
+    }
+    
+    
+}
