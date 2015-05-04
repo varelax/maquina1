e87ba353924b2010d8bd751f76ce033527c38c96
Luis Varela <lvarela@iesfsl.es>
2015-05-04 09:01:12 +0200

    Implementada clase ContadorDeBotes Jose

--- src/maquinabebidas/DispensadorBotes.java
+++ src/maquinabebidas/DispensadorBotes.java
@@ -1,139 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maquinabebidas;
 
+/**
+ *
+ * @author 
+ */
+public class DispensadorDeBotes {
 
-import java.util.*;
-
-public class DispensadorBotes {
-
-    private int boteCola;
-    private int boteLimon;
-    private int boteNaranja;
-    private int boteTonica;
-    private int boteAgua;
+    private int botes; //Numero actual de botes.
     
-    public void IniciarDispensadorBotes (int i){
-      boteCola=i;
-      boteLimon=i;
-      boteNaranja=i;
-      boteTonica=i;
-      boteAgua=i;  
-    }
-      
-    public boolean PulsarBoton (){
-        
-        Scanner tec=new Scanner(System.in);
-        int opcion;
-        boolean monedaIn = true;
-        boolean stock= true;
-        do{
-            System.out.println("1- Cola");
-            System.out.println("2- Limon");
-            System.out.println("3- Naranja");
-            System.out.println("4- Tonica");
-            System.out.println("5- Agua");
-            System.out.println("0- ");
-            opcion=tec.nextInt();
-            switch(opcion){
-                case 1:
-                    if(boteCola!=0){
-                        return stock;
-                    }else{
-                        stock=false;
-                        System.out.println(stock);//no me deja  poner otra vez return stock, si lo hago da error el break
-                    }
-                    break;
-                    
-                case 2:
-                    if(boteLimon!=0){
-                        return stock;
-                    }else{
-                        stock=false;
-                        System.out.println(stock);
-                    }
-                    break;
-                case 3:
-                     if(boteNaranja!=0){
-                        return stock;
-                    }else{
-                        stock=false;
-                        System.out.println(stock);
-                    }
-                    break;
-                case 4:
-                     if(boteTonica!=0){
-                        return stock;
-                    }else{
-                        stock=false;
-                        System.out.println(stock);
-                    }
-                    break;
-                case 5:
-                     if(boteAgua!=0){
-                        return stock;
+    /* Inicializa el dispensador y pone i botes en Ã©l*/
+    /* Entrada: int i, numero de botes en el dispensador*/
+    /* Salidas: Ninguna */
 
-                    }else{
-                        stock=false;
-                        System.out.println(stock);
-                    }
-                    break;
-                case 0:
-                    
-                    monedaIn = false;
-                    break;
-            
-            }
-             
-        }while(monedaIn=true);
-        return false;
-        
+    public void IniciarDispensadorDeBotes(int i) {
+       botes = i;
     }
-   
-    public int BotesDiponibles (){
-        Scanner tec=new Scanner(System.in);
-        int opcion;
-        boolean monedaIn = true;
-        int nuevostock=0;
-        do{
-            /*System.out.println("1- Cola");
-            System.out.println("2- Limon");
-            System.out.println("3- Naranja");
-            System.out.println("4- Tonica");
-            System.out.println("5- Agua");
-            System.out.println("0- ");*/
-            opcion=tec.nextInt();
-            switch(opcion){
-                case 1:
-                    boteCola--;
-                    nuevostock=boteCola;
-                    break;
-                    
-                case 2:
-                    boteLimon--;
-                    nuevostock=boteLimon;
-                    break;
-                    
-                case 3:
-                    boteNaranja--;
-                    nuevostock=boteNaranja;
-                    break;
-                case 4:
-                    boteTonica--;
-                    nuevostock=boteTonica;
-                    break;
-                    
-                case 5:
-                     boteAgua--;
-                     nuevostock=boteAgua;
-                    break;
-                    
-                case 0:
-                    monedaIn = false;
-                    break;
-            
-            }
-             
-        }while(monedaIn=true);
-        return nuevostock;
-      
+    /* Intenta dar un bote, devuelve true si es posible y falso
+     si no hay
+     botes */
+    /* Entrada: Ninguna */
+    /* Salidas: bool, true si entrega el bote */
+
+    public boolean PulsarBoton() {
+        boolean correcto=false;
+        if (botes > 0) {
+            botes-=1;
+            correcto=true;
+        }  
+        return correcto;
+
     }
-    
+    /*Muestra el numero de botes disponibles en el
+     dispensador*/
+    /* Entrada: Ninguna */
+    /* Salidas: int, el numero de botes */
+
+    public int BotesDisponibles() {
+        return botes;
+    }
 }
